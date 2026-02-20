09cb1e433066ece356924b913e75471e1fa0cfaf
psantipap <git@LCIT-GITLAB-01.lcit.com:PSantipap1/LCIT_EDI_APP.git>
2026-02-20 08:25:55 +0700

    Edit test case

--- task2_brackets.test.ts
+++ task2_brackets.test.ts
@@ -5,7 +5,7 @@ import { isValidBrackets } from './bracketValidator';
 test('Task 2: ตรวจสอบความถูกต้องของวงเล็บ', () => {
   // ค่าเหล่านี้อ้างอิงตาม "ตัวอย่างผลลัพธ์" ในรูป image_2bbf6d.png
   assert.strictEqual(isValidBrackets("()"), true);       //
-  assert.strictEqual(isValidBrackets("([])"), false);     // (โจทย์ต้องการ false)
+  assert.strictEqual(isValidBrackets("([]]"), false);     //
   assert.strictEqual(isValidBrackets("([{}])"), true);    //
   assert.strictEqual(isValidBrackets("([[{}]]]"), false); //
   assert.strictEqual(isValidBrackets(")"), false);       //
